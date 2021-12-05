4a0d4e23be9e8ab7c216de03dafd25d8b42d6904
Rushikesh <rushipatil241297@gmail.com>
2021-12-05 11:57:59 +0530

    Day 11 and 12

---
+++ Csharp_Assignments/Web_4_Assigment_Day_11_12_mvc/Program_1_mvc_CRUD/mvcApp1/BAL/AppBAL.cs
@@ -0,0 +1,73 @@
+﻿using mvcApp1.DAL;
+using mvcApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace mvcApp1.BAL
+{
+    public class AppBAL
+    {
+        AppDAL dal = new AppDAL();
+        
+        // Methods ---------
+        public List<EmpModel> GetDeptList()
+        {
+            SqlDataReader reader = dal.GetDepartment();
+            List<EmpModel> list_1 = new List<EmpModel>();
+            while (reader.Read())
+            {
+                EmpModel em = new EmpModel();
+                em.empno = Convert.ToInt32(reader["empno"].ToString());
+                em.empname = reader["empname"].ToString();
+                em.designation = reader["designation"].ToString();
+                em.deptname = reader["deptname"].ToString();
+                list_1.Add(em);
+            }
+            return list_1;
+        }
+
+
+
+        public EmpModel GetDeptusingdno(int empno)
+        {
+            SqlDataReader reader = dal.GetDepartmentUsingDno(empno);
+            EmpModel em = null;
+            while (reader.Read())
+            {
+                em = new EmpModel();
+                em.empno = Convert.ToInt32(reader["empno"].ToString());
+                em.empname = reader["empname"].ToString();
+                em.designation = reader["designation"].ToString();
+                em.deptname = reader["deptname"].ToString();
+            }
+            return em;
+        }
+
+        public int InsertDepartment(EmpModel em)
+        {
+            int res = dal.InsertDepartment(em.empno, em.empname, em.designation, em.deptname);
+            return res;
+
+        }
+
+        public int UpdateDepartment(EmpModel em)
+        {
+            int res = dal.UpdateDepartment(em.empno, em.empname, em.designation, em.deptname);
+            return res;
+
+        }
+
+        public int DeleteDepartment(int empno)
+        {
+            int res = dal.DeleteDepartment(empno);
+            return res;
+
+        }
+
+
+    }
+}
---
+++ Csharp_Assignments/Web_4_Assigment_Day_11_12_mvc/Program_1_mvc_CRUD/mvcApp1/Models/EmpModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvcApp1.Models
+{
+    public class EmpModel
+    {
+
+
+        [Required (ErrorMessage ="Emp can not be blank")]
+        [RegularExpression("\\d{2}", ErrorMessage= "no more than 3 digit")]
+
+        public int empno { get; set; }
+
+        [Required(ErrorMessage="emp name can not be blank")]
+        [StringLength(50,MinimumLength =2)]
+
+        public string empname { get; set; }
+
+        [Required(ErrorMessage = "designation can not be blank")]
+        [StringLength(50, MinimumLength = 2)]
+        public string designation { get; set; }
+
+
+        [Required(ErrorMessage = "Department name can not be blank")]
+        [StringLength(50, MinimumLength = 2)]
+
+        public string deptname { get; set; }
+    }
+}
